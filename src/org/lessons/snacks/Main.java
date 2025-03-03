4f6faeb86f061f43d4bfea867b3ce226a51e6d01
Simone-Domenici <domenici.simone00@gmail.com>
2025-03-03 17:27:23 +0100

    Add Snack 1: Studente

---
+++ src/org/lessons/snacks/Main.java
@@ -0,0 +1,8 @@
+package org.lessons.snacks;
+
+public class Main {
+    public static void main(String[] args) {
+        Studente Bitta = new Studente("Anselmo", "Bitta", 24);
+        System.out.println(Bitta.fullInfo());
+    }
+}
--- src/org/lessons/snacks/Studente.java
+++ src/org/lessons/snacks/Studente.java
@@ -1,5 +1,50 @@
 package org.lessons.snacks;
 
 public class Studente {
+    private String name;
+    private String surname;
+    private int age;
+
+    public Studente()  {
+        this.name = "";
+        this.surname = "";
+        this.age = 0;
+    }
+
+    public Studente(String name, String surname, int age)  {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName( String name ) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return this.surname;
+    }
+
+    public void setSurname( String surname ) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge( int age ) {
+        this.age = age;
+    }
+
+    // metodi aggiuntivi
+
+    public String fullInfo() {
+        return this.name + " " + this.surname + ", " + this.age + " anni";
+    }
 
 }
